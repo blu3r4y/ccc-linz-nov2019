94211e9e4b0ce26619d101bb4dbb2ad05679c39e
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2019-11-08 15:57:31 +0100

    Level 2

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pprint import pprint
 
 from contest import solve
@@ -6,17 +7,25 @@ import numpy as np
 
 
 def load(data):
-    return np.array([e.split(" ") for e in data[1:]], dtype=int)
+    data = [list(map(int, e.split(" "))) for e in data[1:]]
+    grid = np.array([d[::2] for d in data], dtype=int)
+    countries = np.array([d[1::2] for d in data], dtype=int)
+    return {
+        "grid": grid,
+        "countries": countries
+    }
 
 
 if __name__ == "__main__":
-    level, quests = 1, 5
-    for q in range(1, quests + 1):
+    level, quests = 2, 5
+    for q in ["example"] + list(range(1, quests + 1)):
         input_file = r'..\data\level{0}\level{0}_{1}.in'.format(level, q)
         output_file = os.path.splitext(input_file)[0] + ".out"
 
         with open(input_file, 'r') as fi:
             data = load(fi.read().splitlines())
+            if data is None:
+                sys.exit(0)
             # pprint(data)
 
             print("=== Input {}".format(q))
@@ -26,4 +35,4 @@ if __name__ == "__main__":
             pprint(result)
 
             with open(output_file, 'w+') as fo:
-                fo.write(" ".join(map(str, result)))
+                fo.write(result)
--- src/contest.py
+++ src/contest.py
@@ -1,5 +1,35 @@
-import math
+import numpy as np
+
+
+def neighbours(x, y):
+    offsets = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    neigh = [(x + dx, y + dy) for dx, dy in offsets]
+    return neigh
+
+
+def neighbours_values(x, y, grid):
+    if is_edge(x, y, grid.shape):
+        return [-1]
+    return [grid[nx, ny] for nx, ny in neighbours(x, y)]
+
+
+def is_edge(x, y, shape):
+    return x == 0 or y == 0 or (x == shape[0] - 1) or (y == shape[1] - 1)
 
 
 def solve(data):
-    return data.min(), data.max(), math.floor(data.mean())
+    _gr, _co = data["grid"], data["countries"]
+    _grs = _gr.shape
+    num_countries = len(np.unique(_co))
+
+    num_borders = [0] * num_countries
+
+    for (x, y), cid in np.ndenumerate(_co):
+        edge = is_edge(x, y, _grs)
+        nids = neighbours_values(x, y, _co)
+
+        border = any([nid != cid for nid in nids])
+
+        num_borders[cid] += edge or border
+
+    return "\n".join(map(str, num_borders))
